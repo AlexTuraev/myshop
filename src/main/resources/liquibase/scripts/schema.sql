fa0e95aba8b194097ab1d04dc4f44684fa2b1627
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-04-09 14:08:52 +0300

    структура БД

--- src/main/resources/liquibase/scripts/schema.sql
+++ src/main/resources/liquibase/scripts/schema.sql
@@ -1,8 +1,44 @@
 -- liquibase formatted sql
 
 -- changeset alex turaev:1
-drop table if exists items;
+-- drop table if exists items;
+-- drop table if exists users;
 
+-- товары
 create table if not exists items(
-    id bigserial primary key
+    id bigserial primary key,
+    title varchar(255) not null,
+    description text,
+    price numeric(10, 2),
+    count int,
+    image_type varchar(50),
+    image bytea
+    );
+
+-- пользователи
+create table if not exists users(
+    id bigserial primary key,
+    login varchar(255)
+    );
+
+-- корзина
+create table if not exists users_items(
+    item_id bigint,
+    count_item int,
+    user_id bigint,
+
+    foreign key (item_id) references items(id) on delete cascade,
+    foreign key (user_id) references users(id) on delete cascade
+    );
+
+-- заказы
+create table if not exists orders(
+    order_id bigint,
+    item_id bigint,
+    count_item int,
+    price numeric(10, 2),
+    user_id bigint,
+
+    foreign key (item_id) references items(id),
+    foreign key (user_id) references users(id)
     );
