cd6a0177f9efea00938d7c1a5f8555415292a767
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-04-10 18:44:03 +0300

    небольшие правки(2)

--- src/main/java/org/tasks/myshop/dao/repository/ItemRespository.java
+++ src/main/java/org/tasks/myshop/dao/repository/ItemRespository.java
@@ -2,14 +2,10 @@ package org.tasks.myshop.dao.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.tasks.myshop.dao.model.ItemEntity;
-
-import java.util.List;
 
 @Repository
 public interface ItemRespository extends JpaRepository<ItemEntity, Long> {
