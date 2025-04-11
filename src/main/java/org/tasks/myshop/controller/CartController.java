dccaecf89a86054ce942822f1f94497a676221e1
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-04-11 03:28:27 +0300

    подготовка страницы cart и контроллера CartController

--- src/main/java/org/tasks/myshop/controller/CartController.java
+++ src/main/java/org/tasks/myshop/controller/CartController.java
@@ -1,10 +1,20 @@
 package org.tasks.myshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.tasks.myshop.service.CartService;
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
+    private CartService cartService;
 
+    @GetMapping("/{id}")
+    public String getCartByCartId(@PathVariable("id") Long cartId, Model model) {
+        return null;
+//        return "cart";
+    }
 }
